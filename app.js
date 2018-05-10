a686a61d0096f217cf7f4e71162e2674e6e789c8
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2018-05-10 01:37:39 -0500

    Added bill search functionality to test route

--- app.js
+++ app.js
@@ -22,9 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-var api_key = process.env.API_KEY;
-console.log(api_key);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+var api_key = process.env.API_KEY;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test/:query', function(req, res, next) {
+  var options = {
+    url: 'https://api.propublica.org/congress/v1/bills/search.json?query=' + req.params.query,
+    headers: {
+      'X-API-Key': api_key
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      // for (var i = 0; i < data.results[0].bills.length; i++) {
+      //     console.log(data.results[0].bills[i].bill_id);
+      // };
+      var bills =  data.results[0].bills;
+      res.render('test', {data: bills});
+    }
+  }
+
+  request(options, callback);
+
 });
 
 module.exports = router;
