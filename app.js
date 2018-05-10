d9515bb97ee5c0823b179d8263e98b86fdeebbf7
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2018-05-09 23:10:23 -0500

    Testing environment variables and gitignore

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+var api_key = process.env.API_KEY;
+console.log(api_key);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
