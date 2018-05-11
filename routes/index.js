518f97cfc6a520d8b334e3c0751072e9d6116ec8
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2018-05-11 17:46:43 -0500

    Fixed search

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var sortJsonArray = require('sort-json-array');
+var datetime = require('node-datetime');
 
 var api_key = process.env.PROPUBLICA_API_KEY;
 
@@ -21,9 +22,16 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
-router.get('/search', function(req, res, next) {
-  // req.session.history.push(req.params.query);
+/* POST search results from home page to search route */
+router.post('/submit', function(req, res, next) {
+  var query = req.body.query;
+  var sortType = req.body.sortType;
+  res.redirect('/search/' + sortType + "/" + query);
+});
 
+/* Search for related bills and return list */
+router.get('/search/:sortType/:query', function(req, res, next) {
+  console.log(req.params.sortType);
   var options = {
     url: 'https://api.propublica.org/congress/v1/bills/search.json?query=' + req.params.query,
     headers: {
@@ -31,15 +39,17 @@ router.get('/search', function(req, res, next) {
     }
   };
 
-  console.log(req.params.query);
-
   function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
       var bills =  data.results[0].bills;
+
       if (req.params.sortType == "co-sponsors") {
         var sorted_bills = sortJsonArray(bills, 'cosponsors', 'des');
       }
+      // else if (req.params.sortType == "recent-action") {
+      //   var past =  bills.latest_major_action_date+ ' 00:00:00';
+      // }
       else {
         var sorted_bills = bills;
       }
