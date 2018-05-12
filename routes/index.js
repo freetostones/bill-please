d428380e4d0c36afcc4d748141c8b0590eb2cda5
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2018-05-12 14:08:34 -0500

    Added working async to api calls with promises

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var request = require('request');
 var sortJsonArray = require('sort-json-array');
 var datetime = require('node-datetime');
 var hbs = require('hbs');
-var asyncHandler = require('express-async-handler');
 
 var api_key = process.env.PROPUBLICA_API_KEY;
 
@@ -43,7 +42,8 @@ router.post('/submit', function(req, res, next) {
 });
 
 /* Search for related bills and return list */
-router.get('/search/:sortType/:query', asyncHandler( async function(req, res, next) {
+router.get('/search/:sortType/:query', function(req, res, next) {
+  var sorted_bills = [];
   var options = {
     url: 'https://api.propublica.org/congress/v1/bills/search.json?query=' + req.params.query,
     headers: {
@@ -56,94 +56,101 @@ router.get('/search/:sortType/:query', asyncHandler( async function(req, res, ne
       var data = JSON.parse(body);
       var bills =  data.results[0].bills;
 
-      // check if the bill is active and if it has been passed at all
+      promise_list = [];
       for (i = 0; i < bills.length; i++) {
+        var id =  bills[i].bill_id.slice(0,-4);
+        promise_list.push(getRelatedBillsPromise(id, bills[i]));
+      }
+
+      Promise.all(promise_list).then( function(){
+        if (req.params.sortType == "Co-Sponsors") {
+          var sorted_bills = sortJsonArray(bills, 'cosponsors', 'des');
+        }
+        else if (req.params.sortType == "Recent-Action") {
+          var sorted_bills = sortJsonArray(bills, 'date', 'des');
+        }
+        else if (req.params.sortType == "Related-Bills") {
+          var sorted_bills = sortJsonArray(bills, 'numRelatedBills', 'des');;
+        }
+        else {
+          var sorted_bills = bills;
+        }
+        res.render('search', {query: req.params.query, data: sorted_bills });
+
+      });
+
+    }
+
+  }
+
+    request(options, callback);
+
+});
+
+function getRelatedBillsPromise(id, billsObj) {
+  return new Promise(function(resolve, reject) {
+    var numRelatedBills = 0;
+    var options = {
+      url: 'https://api.propublica.org/congress/v1/115/bills/' + id + '/related.json',
+      headers: {
+        'X-API-Key': api_key
+      }
+    };
+
+    function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        var numBills =  data.results[0].related_bills.length;
+        billsObj.numRelatedBills = numBills;
 
         // check if summary is blank
-        if (bills[i].summary == ""){
-          bills[i].summary = "No summary available"
+        if (billsObj.summary == ""){
+          billsObj.summary = "No summary available"
         }
-        // assign number of related bills
-        var id =  bills[i].bill_id.slice(0,-4);
-        var numBills = getRelatedBills(id);
-        bills[i].numRelatedBills = numBills;
 
         // assign integer date value
-        var past =  bills[i].latest_major_action_date + ' 00:00:00';
+        var past =  billsObj.latest_major_action_date + ' 00:00:00';
         var pastDateTime = datetime.create(past);
         var pastNow = pastDateTime.now();
-        bills[i].date = pastNow;
+        billsObj.date = pastNow;
 
-        if (bills[i].active == true) {
-          bills[i].status = "Still active";
-          if (bills[i].house_passage == null) {
-            bills[i].house = "Not passed";
+        if (billsObj.active == true) {
+          billsObj.status = "Still active";
+          if (billsObj.house_passage == null) {
+            billsObj.house = "Not passed";
           } else {
-            bills[i].house = "Passed on " + bills[i].house_passage;
+            billsObj.house = "Passed on " + billsObj.house_passage;
           }
 
-          if (bills[i].senate_passage == null) {
-            bills[i].senate = "Not passed";
+          if (billsObj.senate_passage == null) {
+            billsObj.senate = "Not passed";
           } else {
-            bills[i].senate = "Passed on " + bills[i].senate_passage;
+            billsObj.senate = "Passed on " + billsObj.senate_passage;
           }
         }
         else {
-          bills[i].status = "No longer active";
-          if (bills[i].house_passage == null) {
-            bills[i].house = "Not passed";
+          billsObj.status = "No longer active";
+          if (billsObj.house_passage == null) {
+            billsObj.house = "Not passed";
           } else {
-            bills[i].house = "Passed on " + bills[i].house_passage;
+            billsObj.house = "Passed on " + billsObj.house_passage;
           }
 
-          if (bills[i].senate_passage == null) {
-            bills[i].senate = "Not passed";
+          if (billsObj.senate_passage == null) {
+            billsObj.senate = "Not passed";
           } else {
-            bills[i].senate = "Passed on " + bills[i].senate_passage;
+            billsObj.senate = "Passed on " + billsObj.senate_passage;
           }
         }
-      }
-
-      // sort array of bills based off of sort type parameter
-      if (req.params.sortType == "Co-Sponsors") {
-        var sorted_bills = sortJsonArray(bills, 'cosponsors', 'des');
-      }
-      else if (req.params.sortType == "Recent-Action") {
-        var sorted_bills = sortJsonArray(bills, 'date', 'des');
-      }
-      else if (req.params.sortType == "Related-Bills") {
-        var sorted_bills = sortJsonArray(bills, 'numRelatedBills', 'des');;
+        resolve("Good job");
       }
       else {
-        var sorted_bills = bills;
+        reject(Error("It broke"))
       }
-
-      res.render('search', {query: req.params.query, data: sorted_bills });
     }
-  }
+    request(options, callback);
+  })
 
-  request(options, callback);
-
-}));
-
-// get number of related bills based on given id of a bill
-function getRelatedBills(id) {
-  var numRelatedBills = 0;
-  var options = {
-    url: 'https://api.propublica.org/congress/v1/115/bills/' + id + '/related.json',
-    headers: {
-      'X-API-Key': api_key
-    }
-  };
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      var numRelatedBills =  data.results[0].related_bills.length;
-      return numRelatedBills;
-    }
-  }
-  request(options, callback);
-}
+} 
 
 module.exports = router;
